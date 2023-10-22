ad42fe4be025672ca732e7ddd07f78174cd15d99
mohamedaminenouri <mohamedamine.nouri@esprit.tn>
2023-10-22 14:22:09 +0200

    creation branch first and class userr

--- src/main/java/org/example/entities/User.java
+++ src/main/java/org/example/entities/User.java
@@ -1,2 +1,8 @@
-package org.example.entities;public class User {
+package org.example.entities;
+
+public class User {
+    public int id;
+    public String first_name;
+    public String last_name;
+
 }
